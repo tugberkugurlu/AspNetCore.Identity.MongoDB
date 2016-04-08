f5025ea638fcf077ecdd2ae0a8062af6c7731534
Tugberk Ugurlu <tugberk@outlook.com>
2016-04-08 14:29:55 +0100

    added travis CI build config

---
+++ scripts/set-build-version.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+baseVersion=0.0.0-0
+if semver $(git tag -l) &>/dev/null
+then
+    baseVersion=$(semver $((semver $(git tag -l)) | tail -n1) -i prerelease)
+fi
+
+if [ -z "$TRAVIS_TAG" ];
+then
+    if [ -z "$TRAVIS_BRANCH" ];
+    then
+        # can add the build metadata to indicate this is pull request build
+        echo export PROJECT_BUILD_VERSION="$baseVersion.$TRAVIS_BUILD_NUMBER";
+    else
+        # can add the build metadata to indicate this is a branch build
+        echo export PROJECT_BUILD_VERSION="$baseVersion.$TRAVIS_BUILD_NUMBER";
+    fi
+else 
+    if ! semver $TRAVIS_TAG &>/dev/null
+    then
+        # can add the build metadata to indicate this is a tag build which is not a SemVer
+        echo export PROJECT_BUILD_VERSION="$baseVersion.$TRAVIS_BUILD_NUMBER";
+    else
+        echo export PROJECT_BUILD_VERSION=$(semver $TRAVIS_TAG);
+    fi 
+fi
