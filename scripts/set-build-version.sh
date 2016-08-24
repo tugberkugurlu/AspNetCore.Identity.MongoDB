f0fdd33bc7ab82f8b12421039970985f3f957e7f
Tugberk Ugurlu <tugberk@outlook.com>
2016-08-24 22:09:11 +0100

    nuget sucks https://twitter.com/tourismgeek/status/724910645884411904

--- scripts/set-build-version.sh
+++ scripts/set-build-version.sh
@@ -3,7 +3,7 @@
 baseVersion=0.0.0-0
 if semver "ignorethis" $(git tag -l) &>/dev/null
 then
-    baseVersion=$(semver $((semver $(git tag -l)) | tail -n1) -i prerelease)
+    baseVersion=$((semver $(git tag -l)) | tail -n1)
 fi
 
 if [ -z "$TRAVIS_TAG" ];
@@ -11,16 +11,16 @@ then
     if [ -z "$TRAVIS_BRANCH" ];
     then
         # can add the build metadata to indicate this is pull request build
-        echo export PROJECT_BUILD_VERSION="$baseVersion.$TRAVIS_BUILD_NUMBER";
+        echo export PROJECT_BUILD_VERSION="$baseVersion-$TRAVIS_BUILD_NUMBER";
     else
         # can add the build metadata to indicate this is a branch build
-        echo export PROJECT_BUILD_VERSION="$baseVersion.$TRAVIS_BUILD_NUMBER";
+        echo export PROJECT_BUILD_VERSION="$baseVersion-$TRAVIS_BUILD_NUMBER";
     fi
 else 
     if ! semver $TRAVIS_TAG &>/dev/null
     then
         # can add the build metadata to indicate this is a tag build which is not a SemVer
-        echo export PROJECT_BUILD_VERSION="$baseVersion.$TRAVIS_BUILD_NUMBER";
+        echo export PROJECT_BUILD_VERSION="$baseVersion-$TRAVIS_BUILD_NUMBER";
     else
         echo export PROJECT_BUILD_VERSION=$(semver $TRAVIS_TAG);
     fi 
