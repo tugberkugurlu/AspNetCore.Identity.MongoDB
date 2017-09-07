93a2c64a3af7c14ea1e4ee0c8cda7223d83e4a2f
Vladimir <vlkucher07@gmail.com>
2017-09-07 10:35:45 +0300

    commented patch-version.sh since we don't have project.json anymore

--- scripts/patch-version.sh
+++ scripts/patch-version.sh
@@ -1,32 +1,32 @@
-#!/bin/bash
+# #!/bin/bash
 
-for i in "$@"
-do
-case $i in
-    -v=*|--version=*)
-    VERSIONNUMBER="${i#*=}"
-    shift # past argument=value
-    ;;
-    *)
-            # unknown option
-    ;;
-esac
-done
+# for i in "$@"
+# do
+# case $i in
+    # -v=*|--version=*)
+    # VERSIONNUMBER="${i#*=}"
+    # shift # past argument=value
+    # ;;
+    # *)
+            # # unknown option
+    # ;;
+# esac
+# done
 
-echo "VERSION = ${VERSIONNUMBER}"
+# echo "VERSION = ${VERSIONNUMBER}"
 
-if [ -z ${VERSIONNUMBER+x} ]
-then
-    echo "No version is specified, defaulting to 0.0.0"
-    VERSIONNUMBER=0.0.0
-fi
+# if [ -z ${VERSIONNUMBER+x} ]
+# then
+    # echo "No version is specified, defaulting to 0.0.0"
+    # VERSIONNUMBER=0.0.0
+# fi
 
-scriptsDir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-rootDir="$(dirname "$scriptsDir")"
-projectFiles=$rootDir/src/**/project.json
+# scriptsDir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+# rootDir="$(dirname "$scriptsDir")"
+# projectFiles=$rootDir/src/**/project.json
 
-for projectFile in $projectFiles
-do
-    echo "patching $projectFile with version $VERSIONNUMBER"
-    echo $(jq ". + { \"version\": \"$VERSIONNUMBER\" }" <<<$(jq 'del(.version)' <<<"$(cat $projectFile)")) > $projectFile 
-done
+# for projectFile in $projectFiles
+# do
+    # echo "patching $projectFile with version $VERSIONNUMBER"
+    # echo $(jq ". + { \"version\": \"$VERSIONNUMBER\" }" <<<$(jq 'del(.version)' <<<"$(cat $projectFile)")) > $projectFile 
+# done
