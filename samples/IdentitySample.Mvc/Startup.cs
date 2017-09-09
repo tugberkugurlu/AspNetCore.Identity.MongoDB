0cda64df0beb84cf0f34925342c74e8dd1871cf4
Tugberk Ugurlu <tugberk@outlook.com>
2017-09-09 21:12:16 +0100

    MongoUserStore cannot be represented as IUserStore

--- samples/IdentitySample.Mvc/Startup.cs
+++ samples/IdentitySample.Mvc/Startup.cs
@@ -6,9 +6,17 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
 
 namespace IdentitySample
 {
+    public class MongoDbSettings 
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+
     public class Startup
     {
         public Startup(IHostingEnvironment env)
@@ -27,6 +35,16 @@ namespace IdentitySample
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<MongoDbSettings>(Configuration.GetSection("MongoDb"));
+            services.AddSingleton<IUserStore<MongoIdentityUser>>(provider =>
+            {
+                var options = provider.GetService<IOptions<MongoDbSettings>>();
+                var client = new MongoClient(options.Value.ConnectionString);
+                var database = client.GetDatabase(options.Value.DatabaseName);
+ 
+                return new MongoUserStore<MongoIdentityUser>(database);
+            });
+
             services.AddIdentity<MongoIdentityUser>()
                 .AddDefaultTokenProviders();
 
